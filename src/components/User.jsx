16713c7415ecd2b59c78be2f400b9644f5fe46a1
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-10-06 19:03:45 +0330

    User component added to the project

---
+++ src/components/User.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const User = () => {
+    return(
+        <div>
+            <h1>List of users</h1>
+            <ul>
+                <li>users 1</li>
+            </ul>
+        </div>
+    )
+}
+
+export default User;
